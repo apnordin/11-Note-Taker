0918de489c7770ad956b94d0b28b44c04c0ad3b4
Alex Nordin <apnordin@gmail.com>
2020-11-19 20:24:07 -0500

    Fixed css

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,18 @@
 // Dependencies
 var express = require('express');
 var path = require("path");
+// var styles = require("./public/assets/css/styles.css")
 
 // Express App
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-//Data parsing
+// Data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// CSS & Javascript
+app.use(express.static("public"));
 
 //Routing
 
